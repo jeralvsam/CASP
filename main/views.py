4332c6b38787c4b128c3d389626808d086e9713e
Jeral V Sam <jeralvsam@gmail.com>
2026-06-29 00:01:12 +0530

    Save my work

---
+++ main/views.py
@@ -0,0 +1,27 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request,'main/home.html')
+
+def about(request):
+    return render(request, 'main/about.html')
+
+def academics(request):
+    return render(request, 'main/academics.html')
+
+def admissions(request):
+    return render(request, 'main/admissions.html')
+
+def departments(request):
+    return render(request, 'main/departments.html')
+
+def placement(request):
+    return render(request, 'main/placement.html')
+
+def gallery(request):
+    return render(request, 'main/gallery.html')
+
+def contact(request):
+    return render(request, 'main/contact.html')
